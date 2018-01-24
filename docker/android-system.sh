fa0e50597e1ab282f74f2461023e7c991b7cbd86
Marco A L Barbosa <malbarbo@gmail.com>
2018-01-24 20:20:06 -0200

    Hide android-system.sh messages

--- docker/android-system.sh
+++ docker/android-system.sh
@@ -121,4 +121,21 @@ EOF
     rm $0
 }
 
-main "${@}"
+hide_output() {
+  set +x
+  on_err="
+echo ERROR: An error was encountered with the build.
+cat /tmp/build.log
+exit 1
+"
+  trap "$on_err" ERR
+  bash -c "while true; do sleep 30; echo \$(date) - building ...; done" &
+  PING_LOOP_PID=$!
+  $@ &> /tmp/build.log
+  trap - ERR
+  kill $PING_LOOP_PID
+  rm -f /tmp/build.log
+  set -x
+}
+
+hide_output main "${@}"
