981981ac7a6e5557e16ab28a52090a6bfecd5086
Marco A L Barbosa <malbarbo@gmail.com>
2018-01-26 10:22:36 -0200

    Enable arm-unknown-linux-gnueabihf

---
+++ docker/toolchain-armhf.sh
@@ -0,0 +1,3 @@
+mkdir /gcc && cd /gcc
+
+curl -L https://toolchains.free-electrons.com/downloads/releases/toolchains/armv6-eabihf/tarballs/armv6-eabihf--glibc--stable-2017.05-toolchains-1-1.tar.bz2 | tar --strip-components=1 -xj
