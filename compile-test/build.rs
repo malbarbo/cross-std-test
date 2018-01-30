21e0fe496ebbc553398cb562cb8f00ef44ccd34f
Marco A L Barbosa <malbarbo@gmail.com>
2018-01-30 19:31:24 -0200

    Fix compiletest for non windows-gnu targets

--- compile-test/build.rs
+++ compile-test/build.rs
@@ -5,13 +5,17 @@ use std::env;
 fn main() {
     let mut cfg = cc::Build::new();
     if let Some(target) = env::var("COMPILE_TEST_TARGET").ok() {
-        cfg.target(&*target);
+        if target.ends_with("windows-gnu") {
+            cfg.target(&*target)
+               .cargo_metadata(false);
+            println!(
+                "cargo:rustc-link-search=native={}",
+                env::var_os("OUT_DIR").unwrap().into_string().unwrap());
+        }
     }
     cfg.opt_level(0)
         .warnings(false)
         .debug(false)
         .file("src/rust_test_helpers.c")
-        .cargo_metadata(false)
         .compile("rust_test_helpers");
-    println!("cargo:rustc-link-search=native={}", env::var_os("OUT_DIR").unwrap().into_string().unwrap());
 }
