a3a532f957e9e9dc2261511c11b54ca0bdd755d9
Marco A L Barbosa <malbarbo@gmail.com>
2018-07-10 20:33:33 -0300

    Run tests in single step

--- compile-test/build.rs
+++ compile-test/build.rs
@@ -7,9 +7,11 @@ fn main() {
     if let Some(target) = env::var("COMPILE_TEST_TARGET").ok() {
         cfg.target(&*target)
             .cargo_metadata(false);
+        let out = env::var_os("OUT_DIR").unwrap().into_string().unwrap();
         println!(
-            "cargo:rustc-link-search=native={}",
-            env::var_os("OUT_DIR").unwrap().into_string().unwrap());
+            "cargo:rustc-link-search=native={}", out);
+        println!(
+            "cargo:rustc-env=LD_LIBRARY_PATH=/rust/lib/rustlib/{}/lib/:{}", target, out);
     }
     cfg.opt_level(0)
         .warnings(false)
